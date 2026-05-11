f51b49fbabeed493f380cba3b84a7962f58fad67
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-05-11 02:06:13 +0000

    StyleSheet

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,23 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
+    //jsx
     <View style={styles.container}>
-      <Text>Start working on your Mobile App!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.parent}>
+          GLab<Text style={styles.child}>123</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}> Mobile App!</Text>
+      <Text>React Native & Expo Go Mobile Framework!</Text>
+
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
-
+//not CSS - StyleSheet
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +25,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  hello1: {
+    color: "pink",
+    fontSize: 30,
+    borderWidth: 3,
+    borderColor: "green",
+    padding: 5,
+  },
+  header: { fontSize: 30, fontWeight: "700" },
+  parent: { fontSize: 30, color: "red" },
+  child: { fontSize: 20, color: "pink" },
 });
