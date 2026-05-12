3880585d98634f0f6ca1e8d8fad7249599df5012
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-05-12 03:06:31 +0000

    Text Inputs

--- app/App.tsx
+++ app/App.tsx
@@ -1,32 +1,63 @@
 // import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState<string>("GLab");
+  // const [name, setName] = useState<string>("GLab");
   const [test, setTest] = useState({ name: "GLab", age: 42 });
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("GLab");
+  const [age, setAge] = useState<number>(0);
+
   return (
     //jsx
     <View style={styles.container}>
-      <View>
-        <Text style={styles.parent}>
-          GLab
-          <Text style={styles.child}>
-            {name}
-            {test.age}
-            {test.name}
-            {JSON.stringify(test)}
-          </Text>
-        </Text>{" "}
-      </View>
+      <Text style={styles.parent}>
+        GLab
+        <Text style={styles.child}>
+          {name}
+          {test.age}
+          {test.name}
+          {JSON.stringify(test)}
+        </Text>
+      </Text>{" "}
       <Text style={styles.hello1}> Mobile App!</Text>
       <Text>React Native & Expo Go Mobile Framework!</Text>
       {/* <StatusBar style="auto" /> */}
       <Text style={styles.child}>Count = {count}</Text>
       <View>
         {/* <Button title="Increase" onPress={() => alert("Press me")} /> */}
-        <Button title="Increase" onPress={() => setCount(prev => prev + 1)} />
+        <Button title="Increase" onPress={() => setCount((prev) => prev + 1)} />
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 200 }}>Name: {name}</Text>
+          <TextInput
+            // multiline
+            onChangeText={(value) => setName(value)}
+            style={{
+              borderColor: "green",
+              borderWidth: 1,
+              width: 200,
+              padding: 15,
+            }}
+            // autoCapitalize="characters"
+            autoCapitalize="words"
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 200 }}>Age: {age}</Text>
+          <TextInput
+            // multiline
+            onChangeText={(value) => setAge(+value)}
+            style={{
+              borderColor: "green",
+              borderWidth: 1,
+              width: 200,
+              padding: 15,
+            }}
+            keyboardType="numeric"
+            maxLength={2}
+          />
+        </View>
       </View>
     </View>
   );
