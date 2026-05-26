4a88f5eaa5662f36c3bf84cdf698a803b3c04141
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-05-26 05:50:40 +0000

    ADD TODO Button

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,14 @@
 // import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
-  // Button,
+  Button,
   StyleSheet,
   Text,
   View,
   // TextInput,
   ScrollView,
   FlatList,
+  TextInput,
 } from "react-native";
 
 export default function App() {
@@ -27,41 +28,55 @@ export default function App() {
     { id: 8, name: "GLab4", age: 19 },
     { id: 9, name: "GLab5", age: 20 },
   ]);
-  //not CSS - StyleSheet
-
-  const styles = StyleSheet.create({
-    container: {
-      paddingTop: 50,
-      paddingHorizontal: 20,
-      flex: 1,
-      backgroundColor: "#fff",
-      // alignItems: "center",
-      //  justifyContent: "center",
-      textAlign: "center",
-    },
-    hello1: {
-      color: "pink",
-      fontSize: 30,
-      borderWidth: 3,
-      borderColor: "green",
-      padding: 5,
-    },
-    header: {
-      fontWeight: "700",
-      backgroundColor: "orange",
-      // alignItems: "center",
-      //  justifyContent: "center",
-      textAlign: "center",
-      fontSize: 40,
-    },
-    parent: { fontSize: 30, color: "red" },
-    child: { fontSize: 20, color: "pink" },
-  });
-
   return (
     //jsx
     <View style={styles.container}>
       <Text style={styles.header}>ToDo App</Text>
+      {/* form */}
+      <View>
+        <TextInput style={styles.todoInput} />
+        <Button title="ADD TODO" />
+      </View>
+      {/* List Todo */}
+      <View>
+        <Text>List ToDo</Text>
+      </View>
     </View>
   );
 }
+//not CSS - StyleSheet
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    //  justifyContent: "center",
+    textAlign: "center",
+  },
+  hello1: {
+    color: "pink",
+    fontSize: 30,
+    borderWidth: 3,
+    borderColor: "green",
+    padding: 5,
+  },
+  header: {
+    fontWeight: "700",
+    backgroundColor: "orange",
+    // alignItems: "center",
+    //  justifyContent: "center",
+    textAlign: "center",
+    fontSize: 40,
+  },
+  parent: { fontSize: 30, color: "red" },
+  child: { fontSize: 20, color: "pink" },
+  todoInput: {
+    borderBottomColor: "green",
+    borderBottomWidth: 1,
+    padding: 5,
+    margin: 15,
+  },
+});
